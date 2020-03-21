aaae1ee244c041e7b9f2ade69fbd6d191c381036
Richie Bendall <richiebendall@gmail.com>
2020-03-22 03:26:11 +1300

    refactor!: Extract regex parsing logic to seperate module, update dependencies and require Node 10
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.js
+++ index.js
@@ -2,29 +2,20 @@
 
 const { default: ow } = require("ow")
 const coerceRegex = require("regex-parser")
+const parseRegex = require("regex-parse")
 const escapeStringRegexp = require("escape-string-regexp")
-
-// From https://github.com/IonicaBizau/regex-parser.js/blob/a5746771c0ee2e1a3ab1557905ef734c047da0d5/lib/index.js
-const parseRegexp = (regexp) => {
-	const matches = regexp.toString().match(/(\/?)(.+)\1([a-z]*)/i)
-
-	return {
-		main: matches[2],
-		options: matches[3],
-	}
-}
 
 module.exports = (string, delimiter, { anchor = "none" } = {}) => {
 	ow(string, ow.string)
 	ow(delimiter, ow.any(ow.string, ow.regExp))
-	ow(anchor, ow.string.matches(/none|before|after|left|right/))
+	ow(anchor, ow.string.oneOf(["none", "before", "after", "left", "right"]))
 
 	let flags = "g"
 
 	if (typeof delimiter === "string") {
 		delimiter = escapeStringRegexp(delimiter)
 	} else {
-		const { main, options } = parseRegexp(delimiter)
+		const { main, options } = parseRegex(delimiter)
 		delimiter = main
 		flags = options
 	}
